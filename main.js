32ba7d7b365c3e1ff56f5602ca3d504c9a76b8c5
ANAEISA <127767252+ANAEISA@users.noreply.github.com>
2023-10-23 12:25:29 -0300

    Add files via upload

--- main.js
+++ main.js
@@ -2,16 +2,12 @@ function tocaSom(idElementoAudio){
    document.querySelector(idElementoAudio).play();
 }
 const listaDeTeclas=document.querySelectorAll('.tecla');
- let contador = 0;
  //Estrutura de repetição - Enquanto
- while(contador <listaDeTeclas.leight){
+ for(let contador = 0;contador  <listaDeTeclas.lenght;contador++) {
    const tecla= listaDeTeclas[contador];
    const instrumento = tecla.classList[1];
-   const idAudio = '#som_ ${instrumento}';
-   console.log(idAudio);
+   const idAudio = `#som_ ${instrumento}`;
    tecla.onclick = function(){
       tocaSom(idAudio);
     };
-    contador = contador + 1;
-    console.log(contador);
  }
