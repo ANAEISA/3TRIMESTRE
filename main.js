c2029b1763d42abadb771195f0b09b01200a6a51
ANAEISA <127767252+ANAEISA@users.noreply.github.com>
2023-11-08 12:25:01 -0300

    Add files via upload

--- main.js
+++ main.js
@@ -1,34 +1,36 @@
-function tocaSom(idElementoAudio){
+   function tocaSom(idElementoAudio){
    const elemento = document.querySelector(idElementoAudio);
    if (elemento === null) {
     alert('Elemento não encontrado');
    }
+
    if (elemento && elemento.localName === 'audio'){
       elemento.play();
-   }else {
+   }else 
     alert('Elemento não encontrado');
-    }
-   }
-const listaDeTeclas=document.querySelectorAll('.tecla');
+  }
+
+
+  const listaDeTeclas = document.querySelectorAll('.tecla');
  //Estrutura de repetição - Enquanto
  for(let contador = 0;contador<listaDeTeclas.lenght;contador++) {
-   const tecla= listaDeTeclas[contador];
+   const tecla = listaDeTeclas[contador];
    const instrumento = tecla.classList[1];
    const idAudio = `#som_${instrumento}`;
+
    console.log(idAudio);
-   console.log(instrumento);
 
    tecla.onclick = function(){
       tocaSom(idAudio);
     };
-
-    tecla.onkeydown = function (evento) {
+     tecla.onkeydown = function (evento) {
       if (evento.code === 'Enter'|| evento.code === 'Space') {
       tecla.classList.add('ativa');
       }
-  };
+      };
     tecla.onkeydown = function() {
       tecla.classList.remove('ativa');
-    };
+     };
+    console.log(instrumento);
     console.log(contador);
- };
+    }
